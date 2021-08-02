5cccc241f15a5bbb23ed6e38494bbeabc0d2f232
avolschenk <albert.volschenk@gmail.com>
2021-08-02 18:05:00 +0200

    remove sonar cube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 version = "0.0.1-SNAPSHOT"
 
 plugins {
-    id("org.sonarqube") version "2.8"
     id("org.springframework.boot") apply false
     id("io.spring.dependency-management") apply false
     id("org.flywaydb.flyway") apply false
@@ -14,18 +13,5 @@ plugins {
 subprojects {
     repositories {
         mavenCentral()
-    }
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "tinkie101_envel_prac")
-        property("sonar.organization", "tinkie101")
-        property("sonar.host.url", "https://sonarcloud.io")
-
-        property("sonar.java.coveragePlugin", "jacoco")
-        property("sonar.dynamicAnalysis", "reuseReports")
-        property("sonar.language", "kotlin")
-
     }
 }
